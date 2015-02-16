1b235959ca4ba2b7556a7297d65fbcdb1f5f7d37
Ralph-Wang <ralph_ld@aliyun.com>
2015-02-16 15:34:15 +0800

    Site updated at 2015-02-16 07:34:15 UTC

---
+++ js/auto-toc.js
@@ -0,0 +1,26 @@
+;
+
+var sections = $('div.entry-content h2');
+
+var titles = $('ul#markdown-toc a');
+titles[0].className = 'toc-active';
+
+var tops = [];
+
+sections.each(function () {
+  var that = $(this);
+  tops.push(that.offset().top);
+});
+
+$(window).scroll(function () {
+  var that = $(this);
+  var nowTop = that.scrollTop();
+  var i;
+  for (i = 0; i < tops.length; i++) {
+    if (nowTop <= tops[i]) {
+      titles.removeClass();
+      titles[i].className = 'toc-active';
+      break;
+    }
+  }
+});
