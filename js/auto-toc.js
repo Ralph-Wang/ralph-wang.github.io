063498af05d4247ad6ec99a867af393542e9b2ed
Ralph-Wang <ralph_ld@aliyun.com>
2015-02-16 15:52:51 +0800

    Site updated at 2015-02-16 07:52:51 UTC

--- js/auto-toc.js
+++ js/auto-toc.js
@@ -1,26 +1,28 @@
 ;
-
 var sections = $('div.entry-content h2, div.entry-content h3');
 
 var titles = $('ul#markdown-toc a');
-titles[0].className = 'toc-active';
 
-var tops = [];
+if (sections.length == titles.length) {
+  titles[0].className = 'toc-active';
 
-sections.each(function () {
-  var that = $(this);
-  tops.push(that.offset().top);
-});
+  var tops = [];
 
-$(window).scroll(function () {
-  var that = $(this);
-  var nowTop = that.scrollTop();
-  var i;
-  for (i = 0; i < tops.length; i++) {
-    if (nowTop <= tops[i]) {
-      titles.removeClass();
-      titles[i].className = 'toc-active';
-      break;
+  sections.each(function () {
+    var that = $(this);
+    tops.push(that.offset().top);
+  });
+
+  $(window).scroll(function () {
+    var that = $(this);
+    var nowTop = that.scrollTop();
+    var i;
+    for (i = 0; i < tops.length; i++) {
+      if (nowTop <= tops[i]) {
+        titles.removeClass();
+        titles[i].className = 'toc-active';
+        break;
+      }
     }
-  }
-});
+  });
+}
